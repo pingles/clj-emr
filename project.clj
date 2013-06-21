ae71fba948df743e208e0b2a8ec31d50caf589f2
Paul Ingles <paul@oobaloo.co.uk>
2013-06-21 09:48:40 +0100

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clj-emr "0.1.0-SNAPSHOT"
+(defproject clj-emr "0.1.0"
   :description "Clojure library to control Amazon Elastic MapReduce"
   :url "http://github.com/pingles/clj-emr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.amazonaws/aws-java-sdk "1.4.6"]]
+                 [com.amazonaws/aws-java-sdk "1.4.6"]
+                 [org.clojure/java.data "0.1.1"]]
   :profiles {:dev {:dependencies [[expectations "1.4.48"]]
                    :plugins [[lein-expectations "0.0.8"]]}})
