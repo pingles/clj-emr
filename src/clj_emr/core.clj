80a514572593cb43e1ae848391f51e2f48d19e23
Paul Ingles <paul@oobaloo.co.uk>
2013-06-21 12:37:08 +0100

    update list of supported instance types for EMR

--- src/clj_emr/core.clj
+++ src/clj_emr/core.clj
@@ -8,19 +8,16 @@
                                          :m1.medium
                                          :m1.large
                                          :m1.xlarge
-                                         :m3.xlarge
-                                         :m3.2xlarge
-                                         :c1.medium
-                                         :c1.xlarge
-                                         :cc2.8xlarge
                                          :m2.xlarge
                                          :m2.2xlarge
                                          :m2.4xlarge
-                                         :cr1.8xlarge
-                                         :hi1.4xlarge
-                                         :hs1.8xlarge
-                                         :t1.micro
-                                         :cg1.4xlarge})
+                                         :c1.medium
+                                         :c1.xlarge
+                                         :cc2.8xlarge
+                                         :m3.xlarge
+                                         :m3.2xlarge
+                                         :c1.xlarge
+                                         :hi1.4xlarge})
 
 (defn credentials
   [access-key secret-key]
